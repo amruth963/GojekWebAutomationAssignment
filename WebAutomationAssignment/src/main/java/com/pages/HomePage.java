f0c5c84d15b4a0be766cd25c8d1ef616e2e3ea46
amruth963 <amruth963@gmail.com>
2020-07-05 14:19:43 +0530

    Derived methods to enter text and get attribute values of customer details

--- WebAutomationAssignment/src/main/java/com/pages/HomePage.java
+++ WebAutomationAssignment/src/main/java/com/pages/HomePage.java
@@ -53,4 +53,184 @@ public class HomePage extends BrowserUtils{
 		}
 	}
 
+	public boolean enterName(String enterName) {
+		try {	
+			webDriverWait(HomePageObjects.NAMETXTFIELD);
+			isWebElementDisplayed(HomePageObjects.NAMETXTFIELD);
+			sendKeysOnTxtField(HomePageObjects.NAMETXTFIELD, enterName);
+			return flag;
+		}catch (Exception e) {
+			LOGGER.info("Unable to send keys on "+HomePageObjects.NAMETXTFIELD+" due to "+e.getMessage());
+			flag = false;
+			return flag;
+		}
+	}
+
+	public String getNameTxtFieldValue(String attributeName) {
+		String name = null;
+		try {
+			webDriverWait(HomePageObjects.NAMETXTFIELD);
+			isWebElementDisplayed(HomePageObjects.NAMETXTFIELD);
+			name = getAttribute(HomePageObjects.NAMETXTFIELD, attributeName);
+		}catch (Exception e) {
+			LOGGER.info("Unable to get attribute value due to "+e.getMessage());
+		}
+		return name;
+	}
+
+	public boolean enterEmail(String enterName) {
+		try {	
+			webDriverWait(HomePageObjects.EMAILTXTFIELD);
+			isWebElementDisplayed(HomePageObjects.EMAILTXTFIELD);
+			sendKeysOnTxtField(HomePageObjects.EMAILTXTFIELD, enterName);
+			return flag;
+		}catch (Exception e) {
+			LOGGER.info("Unable to send keys on "+HomePageObjects.EMAILTXTFIELD+" due to "+e.getMessage());
+			flag = false;
+			return flag;
+		}
+	}
+
+	public String getEmailTxtFieldValue(String attributeName) {
+		String name = null;
+		try {
+			webDriverWait(HomePageObjects.EMAILTXTFIELD);
+			isWebElementDisplayed(HomePageObjects.EMAILTXTFIELD);
+			name = getAttribute(HomePageObjects.EMAILTXTFIELD, attributeName);
+		}catch (Exception e) {
+			LOGGER.info("Unable to get attribute value due to "+e.getMessage());
+		}
+		return name;
+	}
+
+	public boolean enterPhoneNo(String enterPhoneNo) {
+		try {	
+			webDriverWait(HomePageObjects.PHONENOTXTFIELD);
+			isWebElementDisplayed(HomePageObjects.PHONENOTXTFIELD);
+			sendKeysOnTxtField(HomePageObjects.PHONENOTXTFIELD, enterPhoneNo);
+			return flag;
+		}catch (Exception e) {
+			LOGGER.info("Unable to send keys on "+HomePageObjects.PHONENOTXTFIELD+" due to "+e.getMessage());
+			flag = false;
+			return flag;
+		}
+	}
+
+	public String getPhoneNoTxtFieldValue(String attributeName) {
+		String name = null;
+		try {
+			webDriverWait(HomePageObjects.PHONENOTXTFIELD);
+			isWebElementDisplayed(HomePageObjects.PHONENOTXTFIELD);
+			name = getAttribute(HomePageObjects.PHONENOTXTFIELD, attributeName);
+		}catch (Exception e) {
+			LOGGER.info("Unable to get attribute value due to "+e.getMessage());
+		}
+		return name;
+	}
+
+
+	public boolean enterCity(String enterCity) {
+		try {	
+			webDriverWait(HomePageObjects.CITYTXTFIELD);
+			isWebElementDisplayed(HomePageObjects.CITYTXTFIELD);
+			sendKeysOnTxtField(HomePageObjects.CITYTXTFIELD, enterCity);
+			return flag;
+		}catch (Exception e) {
+			LOGGER.info("Unable to send keys on "+HomePageObjects.CITYTXTFIELD+" due to "+e.getMessage());
+			flag = false;
+			return flag;
+		}
+	}
+
+	public String getCityTxtFieldValue(String attributeName) {
+		String name = null;
+		try {
+			webDriverWait(HomePageObjects.CITYTXTFIELD);
+			isWebElementDisplayed(HomePageObjects.CITYTXTFIELD);
+			name = getAttribute(HomePageObjects.CITYTXTFIELD, attributeName);
+		}catch (Exception e) {
+			LOGGER.info("Unable to get attribute value due to "+e.getMessage());
+		}
+		return name;
+	}
+
+	public boolean enterAddress(String enterAddress) {
+		try {	
+			webDriverWait(HomePageObjects.ADDRESSTXTFIELD);
+			isWebElementDisplayed(HomePageObjects.ADDRESSTXTFIELD);
+			sendKeysOnTxtField(HomePageObjects.ADDRESSTXTFIELD, enterAddress);
+			return flag;
+		}catch (Exception e) {
+			LOGGER.info("Unable to send keys on "+HomePageObjects.ADDRESSTXTFIELD+" due to "+e.getMessage());
+			flag = false;
+			return flag;
+		}
+	}
+
+	public String getAddressTxtFieldValue(String attributeName) {
+		String name = null;
+		try {
+			webDriverWait(HomePageObjects.ADDRESSTXTFIELD);
+			isWebElementDisplayed(HomePageObjects.ADDRESSTXTFIELD);
+			name = getAttribute(HomePageObjects.ADDRESSTXTFIELD, attributeName);
+		}catch (Exception e) {
+			LOGGER.info("Unable to get attribute value due to "+e.getMessage());
+		}
+		return name;
+	}
+
+	public boolean enterPostalCode(String enterPostalCode) {
+		try {	
+			webDriverWait(HomePageObjects.POSTALCODETXTFIELD);
+			isWebElementDisplayed(HomePageObjects.POSTALCODETXTFIELD);
+			sendKeysOnTxtField(HomePageObjects.POSTALCODETXTFIELD, enterPostalCode);
+			return flag;
+		}catch (Exception e) {
+			LOGGER.info("Unable to send keys on "+HomePageObjects.POSTALCODETXTFIELD+" due to "+e.getMessage());
+			flag = false;
+			return flag;
+		}
+	}
+
+	public String getPostalCodeTxtFieldValue(String attributeName) {
+		String name = null;
+		try {
+			webDriverWait(HomePageObjects.POSTALCODETXTFIELD);
+			isWebElementDisplayed(HomePageObjects.POSTALCODETXTFIELD);
+			name = getAttribute(HomePageObjects.POSTALCODETXTFIELD, attributeName);
+		}catch (Exception e) {
+			LOGGER.info("Unable to get attribute value due to "+e.getMessage());
+		}
+		return name;
+	}
+
+	public boolean clickOnCheckOutBtn() {
+		try {
+			webDriverWait(HomePageObjects.CHECKOUTBTN);
+			isWebElementDisplayed(HomePageObjects.CHECKOUTBTN);
+			clickOnElement(HomePageObjects.CHECKOUTBTN);
+			return flag;
+		}catch (Exception e) {
+			LOGGER.info("Unable to click on "+HomePageObjects.CHECKOUTBTN+" due to "+e.getMessage());
+			flag = false;
+			return flag;
+		}
+	}
+
+	public boolean enterCustomerDetailsAndClickOnCheckoutBtn(String name, String email, String phoneno, String city, String address, String postalCode) {
+		try {
+			enterName(name);
+			enterEmail(email);
+			enterPhoneNo(phoneno);
+			enterCity(city);
+			enterAddress(address);
+			enterPostalCode(postalCode);
+			return flag;
+		}catch (Exception e) {
+			LOGGER.info("Error due to "+e.getMessage());
+			flag = false;
+			return flag;
+		}
+	}
+
 }
