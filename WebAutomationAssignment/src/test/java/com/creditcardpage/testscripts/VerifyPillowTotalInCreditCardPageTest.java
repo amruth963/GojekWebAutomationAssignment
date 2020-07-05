36ac36c81260ea557637a9e3f270c689f0a9f007
amruth963 <amruth963@gmail.com>
2020-07-05 23:26:30 +0530

    Added test case to verify pillow total in credit card page

---
+++ WebAutomationAssignment/src/test/java/com/creditcardpage/testscripts/VerifyPillowTotalInCreditCardPageTest.java
@@ -0,0 +1,32 @@
+package com.creditcardpage.testscripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.common.utils.BrowserUtils;
+import com.common.utils.PageConstants;
+import com.pages.CreditCardPage;
+import com.pages.HomePage;
+import com.pages.OrderSummaryPage;
+
+public class VerifyPillowTotalInCreditCardPageTest extends BrowserUtils{
+
+	private HomePage homePage = new HomePage();
+	private OrderSummaryPage orderSummaryPage = new OrderSummaryPage();
+	private CreditCardPage creditCardPage = new CreditCardPage();
+
+	@Test
+	public void verifyPillowTotalInCreditCardPage() {
+		homePage.launchUrl();
+		Assert.assertEquals(homePage.verifyPillowTitle(), PageConstants.MIDTRANSPILLOWTITLE);
+		Assert.assertEquals(homePage.clickOnBuyNowBtn(), true);
+		homePage.enterCustomerDetailsAndClickOnCheckoutBtn(PageConstants.USERNAME, PageConstants.EMAIL, PageConstants.PHONENO, PageConstants.CITY, PageConstants.ADDRESS, PageConstants.POSTALCODE);
+		switchiFrame();
+		String totalPriceInOrderSummaryPage = orderSummaryPage.verifyPillowTotalOnOrderSummaryPage();
+		orderSummaryPage.clickOnContinueBtn();
+		orderSummaryPage.selectCreditCardPaymentOption();
+		String totalPriceInCreditCardPage = creditCardPage.verifyTotalAmountInCreditCardPage();
+		Assert.assertEquals(totalPriceInCreditCardPage, totalPriceInOrderSummaryPage);
+	}
+
+}
