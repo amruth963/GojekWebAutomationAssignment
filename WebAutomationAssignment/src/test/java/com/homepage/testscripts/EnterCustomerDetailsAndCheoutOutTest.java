9d74bf28bb7adefac6e4bab380ad9a924b14f65b
amruth963 <amruth963@gmail.com>
2020-07-05 21:15:16 +0530

    Test case to enter customer details and click on check out button

---
+++ WebAutomationAssignment/src/test/java/com/homepage/testscripts/EnterCustomerDetailsAndCheoutOutTest.java
@@ -0,0 +1,23 @@
+package com.homepage.testscripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.common.utils.BrowserUtils;
+import com.common.utils.PageConstants;
+import com.pages.HomePage;
+
+public class EnterCustomerDetailsAndCheoutOutTest extends BrowserUtils {
+
+	private HomePage homePage = new HomePage();
+
+	@Test(description = "Enter customer details and checout out")
+	public void enterCustomerDetailsAndCheckOut() {
+		homePage.launchUrl();
+		Assert.assertEquals(homePage.verifyPillowTitle(), PageConstants.MIDTRANSPILLOWTITLE);
+		Assert.assertEquals(homePage.clickOnBuyNowBtn(), true);
+		boolean flag = homePage.enterCustomerDetailsAndClickOnCheckoutBtn(PageConstants.USERNAME, PageConstants.EMAIL, PageConstants.PHONENO, PageConstants.CITY, PageConstants.ADDRESS, PageConstants.POSTALCODE);
+		Assert.assertEquals(flag, true);
+	}
+
+}
