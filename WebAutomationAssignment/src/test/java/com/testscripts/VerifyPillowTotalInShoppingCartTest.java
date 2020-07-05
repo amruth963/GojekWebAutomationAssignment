0877f7e72c47531efc71ba516a10d36e74e3a107
amruth963 <amruth963@gmail.com>
2020-07-05 14:22:41 +0530

    Added test case to assert total pillow price between home and shopping cart screen

---
+++ WebAutomationAssignment/src/test/java/com/testscripts/VerifyPillowTotalInShoppingCartTest.java
@@ -0,0 +1,23 @@
+package com.testscripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.common.utils.BrowserUtils;
+import com.pageobjects.HomePageObjects;
+import com.pages.HomePage;
+
+public class VerifyPillowTotalInShoppingCartTest extends BrowserUtils{
+
+	private HomePage homePage = new HomePage();
+
+	@Test(description = "Verify pillow price between home and shopping cart screen")
+	public void verifyPillowTotalInShoppingCart() {
+		homePage.launchUrl();
+		String totalPriceInHomePage = homePage.getText(HomePageObjects.TOTALAMOUNTONHOMESCREEN);
+		homePage.clickOnBuyNowBtn();
+		String totalPriceInCartPage = homePage.getText(HomePageObjects.TOTALAMOUNTONCART);
+		Assert.assertEquals(totalPriceInCartPage, totalPriceInHomePage);
+	}
+
+}
