d831e12722a8ed34817970ad202236f0ae867b7c
amruth963 <amruth963@gmail.com>
2020-07-05 14:17:06 +0530

    Added customer details for assertions

--- WebAutomationAssignment/src/main/java/com/common/utils/PageConstants.java
+++ WebAutomationAssignment/src/main/java/com/common/utils/PageConstants.java
@@ -1,7 +1,13 @@
 package com.common.utils;
 
 public class PageConstants {
-	
+
 	public static final String MIDTRANSPILLOWTITLE = "Midtrans Pillow";
+	public static final String USERNAME = "Test User";
+	public static final String EMAIL = "test@grr.la";
+	public static final String PHONENO = "8765454321";
+	public static final String CITY = "Bangalore";
+	public static final String ADDRESS = "Bangalore South";
+	public static final String POSTALCODE = "560041";
 
 }
