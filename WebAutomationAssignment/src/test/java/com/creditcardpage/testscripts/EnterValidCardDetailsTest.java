84257df4f636b57fd7bf2ed5129ef22100889b88
amruth963 <amruth963@gmail.com>
2020-07-06 11:51:32 +0530

    Test case to verify valid transaction

---
+++ WebAutomationAssignment/src/test/java/com/creditcardpage/testscripts/EnterValidCardDetailsTest.java
@@ -0,0 +1,39 @@
+package com.creditcardpage.testscripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.common.utils.BrowserUtils;
+import com.common.utils.ConfigReader;
+import com.common.utils.PageConstants;
+import com.pages.CreditCardPage;
+import com.pages.HomePage;
+import com.pages.OrderSummaryPage;
+
+public class EnterValidCardDetailsTest extends BrowserUtils{
+
+	private HomePage homePage = new HomePage();
+	private OrderSummaryPage orderSummaryPage = new OrderSummaryPage();
+	private CreditCardPage creditCardPage = new CreditCardPage();
+	private ConfigReader configReader = new ConfigReader();
+
+	@Test
+	public void enterValidCardDetails() {
+		homePage.launchUrl();
+		Assert.assertEquals(homePage.verifyPillowTitle(), PageConstants.MIDTRANSPILLOWTITLE);
+		Assert.assertEquals(homePage.clickOnBuyNowBtn(), true);
+		homePage.enterCustomerDetailsAndClickOnCheckoutBtn(PageConstants.USERNAME, PageConstants.EMAIL, PageConstants.PHONENO, PageConstants.CITY, PageConstants.ADDRESS, PageConstants.POSTALCODE);
+		switchiFrameOne();
+		orderSummaryPage.clickOnContinueBtn();
+		orderSummaryPage.selectCreditCardPaymentOption();
+		creditCardPage.enterCardNumber(configReader.getStaticProperty("validcardnumber"));
+		creditCardPage.enterExpiryDate(configReader.getStaticProperty("validExpiryDate"));
+		creditCardPage.enterCVV(configReader.getStaticProperty("validcvv"));
+		creditCardPage.clickOnPayNowBtn();
+		creditCardPage.enterOTP(configReader.getStaticProperty("otp"));
+		creditCardPage.clickOnOkBtn();
+		Assert.assertEquals(creditCardPage.getTransactionSuccessMsg(), PageConstants.PURCHASESUCCESSMSG);
+	}
+
+
+}
