52cd6b24a2905ab8f0fa65dfd163c81e5c46d761
amruth963 <amruth963@gmail.com>
2020-07-06 11:47:48 +0530

    Added credit card page element identifiers

--- WebAutomationAssignment/src/main/java/com/pageobjects/CreditCardPageObjects.java
+++ WebAutomationAssignment/src/main/java/com/pageobjects/CreditCardPageObjects.java
@@ -5,16 +5,16 @@ import org.openqa.selenium.By;
 public class CreditCardPageObjects {
 
 	public static By TOTALAMOUNTINPAYMENTPAGE = By.xpath("//span[@class='text-amount-amount']");
-	public static By CARDNUMBERTXTFIELD = By.xpath("//*[contains(text(),'Card number')]");
-	public static By EXPIRYDATETXTFIELD = By.xpath("//*[contains(text(),'Expiry date')]");
-	public static By CVVTXTFIELD = By.xpath("//*[contains(text(),'CVV')]");
-	public static By PAYNOWBTN = By.xpath("//*[@id=\"application\"]/div[1]/a/div[1]/span");
+	public static By CARDNUMBERTXTFIELD = By.name("cardnumber");
+	public static By EXPIRYDATETXTFIELD = By.xpath("//input[@type='tel' and @placeholder='MM / YY']");
+	public static By CVVTXTFIELD = By.xpath("//input[@type='tel' and @ inputmode ='numeric']");
+	public static By PAYNOWBTN = By.xpath("//span[contains(text(),'Pay Now')]");
 	public static By ISSUINGBANKPAGE = By.xpath("//h1[contains(text(),'Issuing Bank')]");
-	public static By PASSWORDTXTFIELD = By.id("PaRes");
+	public static By OTPTXTFIELD = By.xpath("//input[@type='password' and @placeholder='112233']");
 	public static By OKBTN = By.xpath("//button[@class='btn btn-sm btn-success']");
-	public static By TRANSACTIONSUCCESSMSG= By.xpath("//div[@class='text-success text-bold']");
+//	public static By TRANSACTIONSUCCESSMSG= By.xpath("//div[@class='text-success text-bold']");
 	public static By TRANSACTIONFAILUREMSG = By.xpath("//div[@class='text-failed text-bold']");
 	public static By TRANSACTIONFAILUREREASON = By.xpath("//div[@class='text-failed']");
-	
+
 
 }
